b970fd7754957a9352fd5d681e23bb883251e62f
Hamid <www.abdulhamid3311@gmail.com>
2022-03-01 12:10:19 +0600

    added phone details

--- js/index.js
+++ js/index.js
@@ -1,36 +1,76 @@
 const searchFood = () => {
     const inputField = document.getElementById('input-search');
     const inputText = inputField.value;
-
+    //clear search field
     inputField.value = '';
-
+    //data load
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
     fetch(url)
         .then(res => res.json())
-        .then(phones => displaySearchResult(phones.data));
+        .then(phones => displaySearchResults(phones.data));
 };
 
-const displaySearchResult = (phones) => {
-    // console.log(phones);
+const displaySearchResults = (phones) => {
+
     const searchResults = document.getElementById('search-result');
+    //clear search result field
     searchResults.textContent = '';
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const col = document.createElement('div');
-
-
         col.innerHTML = `
         <div class="card">
             <img src="${phone.image}" class="card-img-top img-fluid">
             <div class="card-body">
                  <h4 class="card-title">${phone.phone_name}</h4>
                  <h6 class="card-title">Brand name: ${phone.brand}</h6>
-                 <button class="btn btn-primary">Explorer</button>
+                 <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Explorer</button>
+            </div>
+        </div>`
+        searchResults.appendChild(col);
+    })
+};
+
+const loadPhoneDetails = (phoneId) => {
+    // console.log(phoneId);
+    const phoneUrl = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(phoneUrl)
+        .then(res => res.json())
+        .then(details => displayPhoneDetails(details.data))
+};
+
+const displayPhoneDetails = (detail) => {
+    console.log(detail);
+    const phoneDetail = document.getElementById('phone-detail');
+    //clear phone detail
+    phoneDetail.textContent = '';
+    const div = document.createElement('div');
+
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="card w-50 h-50 mx-auto">
+            <img src="${detail.image}" class="card-img-top img-fluid">
+            <div class="card-body">
+                <h5 class="card-title">${detail.name}</h5>
+                <h6 class="card-text">Brand Name: ${detail.brand}</h6>
+                <p class="card-title">${detail.releaseDate}</p>
+                
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">storage: ${detail.mainFeatures.storage}</li>
+                <li class="list-group-item">Display: ${detail.mainFeatures.displaySize}</li>
+                <li class="list-group-item">Chip Set: ${detail.mainFeatures.chipSet}</li>
+                <li class="list-group-item">Memory: ${detail.mainFeatures.memory}</li>
+            </ul>
+
+            <div class="card-body">
+                <a href="#" class="card-link btn btn-primary">Buy Now</a>
+                <a href="#" class="card-link btn btn-primary">add to cart</a>
             </div>
         </div>
-    `
-        searchResults.appendChild(col);
+    `;
+    phoneDetail.appendChild(div);
 
-    })
+
 
 }
